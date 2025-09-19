1f64d8142d4a5dbe63a0e4b766b81e923d7c1913
DatNtPH51025 <datntph51025@gmail.com>
2025-09-19 13:51:08 +0700

    fix ai

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-const { getSheetData, appendSheetData } = require("./googleSheets");
+const { getSheetData, appendSheetData, callGeminiWithSheet } = require("./googleSheets");
 
 const app = express();
 app.use(bodyParser.json());
